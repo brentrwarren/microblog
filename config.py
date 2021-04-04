5cb7e8071a529b68eb64bd7a6ac66a4d49169316
brent <brentrwarren73@gmail.com>
2021-04-04 15:00:07 -0400

    added translation of posts

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -12,4 +13,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
-    POSTS_PER_PAGE = 10
+    LANGUAGES = ['en', 'es']
+    POSTS_PER_PAGE = 25
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
